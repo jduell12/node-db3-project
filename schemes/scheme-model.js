efc0f88939e0c78779e95eb0bf8e4f0a34747422
Jessica <duellje@oregonstate.edu>
2020-08-12 15:13:58 -0400

    added findSteps method

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -14,11 +14,18 @@ function find() {
   return db("schemes");
 }
 
+//returns a single scheme object
 function findById(id) {
   return db("schemes").where({ id }).first();
 }
 
-function findSteps(id) {}
+//returns an array of correctly ordered steps for the given scheme
+//array should include scheme_name not scheme_id
+function findSteps(id) {
+  return db("steps as s")
+    .join("schemes as sc", "s.scheme_id", "sc.id")
+    .select("s.step_number", "s.instructions", "sc.scheme_name");
+}
 
 function add(scheme) {}
 
--- schemes/scheme-router.js
+++ schemes/scheme-router.js
@@ -32,21 +32,23 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// router.get('/:id/steps', (req, res) => {
-//   const { id } = req.params;
+router.get("/:id/steps", (req, res) => {
+  const { id } = req.params;
 
-//   Schemes.findSteps(id)
-//   .then(steps => {
-//     if (steps.length) {
-//       res.json(steps);
-//     } else {
-//       res.status(404).json({ message: 'Could not find steps for given scheme' })
-//     }
-//   })
-//   .catch(err => {
-//     res.status(500).json({ message: 'Failed to get steps' });
-//   });
-// });
+  Schemes.findSteps(id)
+    .then((steps) => {
+      if (steps.length) {
+        res.json(steps);
+      } else {
+        res
+          .status(404)
+          .json({ message: "Could not find steps for given scheme" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get steps" });
+    });
+});
 
 // router.post('/', (req, res) => {
 //   const schemeData = req.body;
